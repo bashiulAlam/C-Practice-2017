4f7908a311b20bc8e45354904e2e06c107884aed
Bashiul Alam <sabab.ndc@gmail.com>
2017-01-23 23:22:31 +0600

    Remove char from string

---
+++ RemoveCharFromString/RemoveCharFromString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveCharFromString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String str = Console.ReadLine();
+            char c = Convert.ToChar(Console.ReadLine());
+
+            int index = str.IndexOf(c);
+            String newStr = str.Remove(index, 1);
+
+            Console.WriteLine("New str : " + newStr);
+            Console.ReadKey();
+        }
+    }
+}
